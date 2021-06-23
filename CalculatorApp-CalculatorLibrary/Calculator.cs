b845ea79375a1bfe38f370f12450d83e5f124ca1
sujithashok <A00218559@mycambrian.ca>
2021-06-23 00:03:51 -0400

    Added 10 tests for Addition

--- CalculatorApp-CalculatorLibrary/Calculator.cs
+++ CalculatorApp-CalculatorLibrary/Calculator.cs
@@ -8,12 +8,29 @@ namespace CalculatorApp_CalculatorLibrary
             return leftNumber + rightNumber;
         }
 
+        static public double sub(double leftNumber, double rightNumber)
+        {
+            return leftNumber - rightNumber;
+        }
+
+        static public double multiple(double leftNumber, double rightNumber)
+        {
+            return leftNumber * rightNumber;
+        }
+
         static public double divide(double leftNumber, double rightNumber)
         {
-            if(rightNumber > 0)
-            return leftNumber / rightNumber;
+            if (rightNumber > 0)
+                return leftNumber / rightNumber;
 
             throw new DivideByZeroException("Cannot divide a number by zero");
         }
+
+        static public double addThree(double leftNumber, double rightNumber, double thirdNumber)
+        {
+            return leftNumber + rightNumber + thirdNumber;
+        }
+
+        
     }
 }
--- CalculatorApp-Test/Calculator Test.cs
+++ CalculatorApp-Test/Calculator Test.cs
@@ -6,18 +6,143 @@ namespace CalculatorApp_Test
     public class CalculatorTest
     {
         [TestMethod]
-        public void AddTwoNumbers()
+        [DataTestMethod]
+        [DataRow(2, 3)]
+        [TestCategory("Addition")]
+        public void AdditionExample(double left, double right)
         {
-            double left = 1;
-            double right = 2;
+            
             double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
-            Assert.AreEqual(3d, result);
+            Assert.AreEqual(5d, result);
 
         }
 
         [TestMethod]
-        public void TestMethod2()
+        [DataTestMethod]
+        [DataRow(-2, -3)]
+        [TestCategory("Addition")]
+        public void AddingOfTwoNegativeNumbers(double left, double right)
         {
+         
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.IsTrue(result < 0);
+
         }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(-2, 3)]
+        [TestCategory("Addition")]
+        public void AddingPositiveAndNegativeNumbers(double left, double right)
+        {
+
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.AreEqual(1d, result);
+
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(-2, 2)]
+        [TestCategory("Addition")]
+        public void AddingOnePositiveAndOneNegativeEqualNumbersresultsInZero(double left, double right)
+        {
+          
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.AreEqual(0, result);
+
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(-2, 5)]
+        [TestCategory("Addition")]
+        public void AddingBiggerPositiveNumberWithSmallerNegativeNumberGivesPositiveResult(double left, double right)
+        {
+           
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(2, -5)]
+        [TestCategory("Addition")]
+        public void AddingBiggerNegativeNumberWithSmallerPositiveNumberGivesNegativeResult(double left, double right)
+        {
+
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.IsTrue(result < 0);
+
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(2, 5)]
+        [TestCategory("Addition")]
+        public void AdditionFollowsAssociativeProperty(double left, double right)
+        {
+
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            double resultReverse = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.AreEqual(result, resultReverse);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(-2, -5)]
+        [TestCategory("Addition")]
+        public void AdditionFollowsAssociativePropertyWithNegativeNumbersToo(double left, double right)
+        {
+
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            double resultReverse = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.AreEqual(result, resultReverse);
+        }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(2, 5, 3)]
+        [TestCategory("Addition")]
+        public void AdditionWillFollowAssociativePropertyEvenWithMoreThanTwoNumbers(double left, double right, double thirdNum)
+        {
+
+            double result = CalculatorApp_CalculatorLibrary.Calculator.addThree(left, right, thirdNum);
+            double result2 = CalculatorApp_CalculatorLibrary.Calculator.addThree(left, thirdNum, right);
+            double result3 = CalculatorApp_CalculatorLibrary.Calculator.addThree(thirdNum, left, right);
+            Assert.AreEqual(result, result2);
+            Assert.AreEqual(result, result3);
+            Assert.AreEqual(result2, result3);
+        }
+
+        
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(0, 5)]
+        [TestCategory("Addition")]
+        public void AddingZeroToAnyNumberWillAlwaysGiveResultSameAsThatNumber(double left, double right)
+        {
+
+            double result = CalculatorApp_CalculatorLibrary.Calculator.add(left, right);
+            Assert.AreEqual(result, right);
+        }
+
+
+
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(3, 1)]
+        [TestCategory("Subtraction")]
+        public void SubtractionExample(double left, double right)
+        {
+
+            double result = CalculatorApp_CalculatorLibrary.Calculator.sub(left, right);
+            Assert.AreEqual(2d, result);
+
+        }
+
     }
 }
